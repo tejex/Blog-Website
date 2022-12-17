0d25cf9e31e33b14d8c514427b14c634ce1d71de
Bamlak Deju Abera <91570985+tejex@users.noreply.github.com>
2022-12-17 13:00:13 +0300

    Incorporating MongoDB into the project
    Added database to the application through mongoDB Cluster and Mongoose.

--- app.js
+++ app.js
@@ -2,62 +2,110 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose =  require("mongoose")
 const ejs = require("ejs");
 var _ = require('lodash')
+
+//******************************************************************************************************************//
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+//******************************************************************************************************************//
 const app = express();
-
-let posts =[]
-
-app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+//******************************************************************************************************************//
 
-app.get("/",function(req,res){
-  res.render("home",{info:homeStartingContent,newPost:posts})
+app.set('view engine', 'ejs');
+
+
+//******************************************************************************************************************//
+//Incorporating mongoDB into the blog website
+
+const password = encodeURIComponent("Gtalebron@23")
+const uri = `mongodb+srv://bamMongo23:${password}@cluster0.wvbvubq.mongodb.net/blogWebsiteData`;
+mongoose.connect(uri, { useNewUrlParser: true})
+
+const postSchema = new mongoose.Schema({
+  title:{
+    required:true,
+    type:String,
+  },
+  content:{
+    required:true,
+    type:String
+  }
 })
 
+const Post = new mongoose.model("Post", postSchema)
+
+//******************************************************************************************************************//
+app.get("/",function(req,res){
+
+
+  Post.find(function(err, foundPosts){
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.render("home",{info:homeStartingContent,newPost:foundPosts})
+    }
+  })
+})
+//******************************************************************************************************************//
 app.get("/about",function(req,res){
   res.render("about",{aboutUs:aboutContent})
 })
-
+//******************************************************************************************************************//
 app.get("/contact",function(req,res){
   res.render("contact",{contactUs:contactContent})
 })
+//******************************************************************************************************************//
 app.get("/compose",function(req,res){
   res.render("compose")
 })
 
-app.get("/post/:postName",function(req,res){
-  const postTitle = _.lowerCase(req.params.postName)
+//******************************************************************************************************************//
+app.get("/post/:postId",function(req,res){
+  const postId = req.params.postId
 
-  posts.forEach(function(post){
-    let title = _.lowerCase(post.pubTitle)
-    if(title === postTitle){
-      res.render("post",{postHeader:post.pubTitle,postContent:post.pubText})
-    }else{
-      console.log("no match")
+  Post.findOne({_id:postId}, function(err,post){
+    if(err){
+      console.log("error");
+    }
+    else{
+      res.render("post",{postHeader:post.title,postContent:post.content})
     }
   })
 })
+//******************************************************************************************************************//
 
 app.post("/compose",function(req,res){
-  console.log(req.body.composeTitle)
-  const post = {
-    pubTitle:req.body.composeTitle,
-    pubText: req.body.composeText
-  }
-  posts.push(post)
-  res.redirect("/")
-})
 
+  const postTitle = req.body.composeTitle;
+  const postContent = req.body.composeText;
+
+  const newPost = new Post({
+    title:postTitle,
+    content:postContent
+  })
+
+  newPost.save(function(err){
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.redirect("/")
+    }
+  });
+})
+//******************************************************************************************************************//
 
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+//******************************************************************************************************************//
